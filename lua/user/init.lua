2913fe017737110bc059deed85dc4cbbcc3e6c4a
Vasilyev, Artem <artem001v@gmail.com>
2023-12-03 23:46:53 +0300

    changed keymappings

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -1,21 +1,4 @@
-function SwitchBetweenHtmlAndTs()
-  local current_file = vim.fn.expand "%:p"
-  local current_extension = vim.fn.expand "%:e"
-
-  local target_extension = ""
-  if current_extension == "html" then
-    target_extension = "ts"
-  elseif current_extension == "ts" then
-    target_extension = "html"
-  else
-    print "Unsupported file type"
-    return
-  end
-
-  local target_file = current_file:gsub(current_extension .. "$", target_extension)
-  vim.cmd("edit " .. target_file)
-end
-
+require "user/utils"
 return {
   colorscheme = "catppuccin",
   lsp = {
