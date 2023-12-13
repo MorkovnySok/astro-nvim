a25bc7db0d2ee358f5d37e73c13c52d85f4bc7dd
Vasilyev, Artem <artemvasilyev@kept.ru>
2023-12-13 17:03:14 +0300

    c sharp support + hardtime

--- lua/user/plugins/community.lua
+++ lua/user/plugins/community.lua
@@ -94,10 +94,52 @@ return {
       vim.tbl_map(function(val) cmp.setup.cmdline(val.type, val) end, opts)
     end,
   },
+  -- CSharp support
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "c_sharp")
+      end
+    end,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "csharpier") end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "csharp_ls") end,
+  },
+  {
+    "mfussenegger/nvim-dap",
+    {
+      "jay-babu/mason-nvim-dap.nvim",
+      opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "coreclr") end,
+    },
+  },
   {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
     opts = {},
+  },
+  {
+    "m4xshen/hardtime.nvim",
+    event = "User AstroFile",
+    opts = {
+      disabled_keys = {
+        ["<Insert>"] = { "", "i" },
+        ["<Home>"] = { "", "i" },
+        ["<End>"] = { "", "i" },
+        ["<PageUp>"] = { "", "i" },
+        ["<PageDown>"] = { "", "i" },
+      },
+      disable_mouse = false,
+    },
+    config = function(_, opts)
+      require("hardtime").setup(opts)
+      require("hardtime").enable()
+    end,
   },
 }
